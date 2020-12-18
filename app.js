5659572ba11b91e7da7b14ea39fbec7826e52e70
uma <info@bhartilodhi.com>
2020-12-17 16:55:11 -0800

    js beautify

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(input)
-{
+function getTranslationURL(input) {
    return serverURL + "?" + "text=" + input;
 }
 
@@ -17,16 +16,14 @@ function errorHandler(error) {
    alert("Server is not responding currently! Try again later.");
 }
 
-function clickHandler()
-{
-var inputText = txtInput.value;
-fetch (getTranslationURL(inputText))
-.then (response => response.json())
-.then (json => {
-   var translatedText = json.contents.translated;
-   outputDiv.innerText = translatedText;
-})
-.catch(errorHandler)
+function clickHandler() {
+   var inputText = txtInput.value;
+   fetch(getTranslationURL(inputText))
+      .then(response => response.json())
+      .then(json => {
+         var translatedText = json.contents.translated;
+         outputDiv.innerText = translatedText;
+      })
+      .catch(errorHandler)
 };
-btnTranslate.addEventListener("click",clickHandler);
-
+btnTranslate.addEventListener("click", clickHandler);
