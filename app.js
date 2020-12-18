8a16d636b02054fb08cf3e532af845c0d0c33992
uma <info@bhartilodhi.com>
2020-12-17 17:14:24 -0800

    js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-alert("Hello.!!")
-var userName = prompt("give your name");
-alert("welcome " + userName);
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
@@ -9,7 +5,7 @@ var outputDiv = document.querySelector("#output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
-   return serverURL + "?" + "text=" + input
+   return serverURL + "?" + "text=" + input;
 }
 
 function errorHandler(error) {
